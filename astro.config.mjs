81665964f41302ede6a7c8256ab3ece2ec8c06c0
Tyler Sax <tylersax@Tylers-MacBook-Air-2.local>
2025-09-17 14:53:59 -0500

    Update base path for FATT-site-demo repository and add node_modules to gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tylersax.github.io',
-  base: '/FATT',
+  base: '/FATT-site-demo',
   integrations: [tailwind()],
   output: 'static',
   build: {
